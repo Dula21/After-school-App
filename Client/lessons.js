0350dd9108780f9e89ea706493ba62d9c6c18e1a
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-10-25 12:18:32 +0400

    lessons list

---
+++ Client/lessons.js
@@ -0,0 +1,21 @@
+const lessons = [
+    {
+        id: 1,
+        title: "Math Tutoring",
+        description: "Enhance your math skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "images/lesson1.jpg",
+        alt: "Math Lesson"
+    },
+    {
+        id: 2,
+        title: "Science Workshop",
+        description: "Hands-on science experience.",
+        price: 150,
+        availableInventory: 3,
+        image: "images/lesson2.jpg",
+        alt: "Science Workshop"
+    }
+    // Add more lessons as needed
+];
