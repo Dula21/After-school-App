1db782d261a7d10fe018843e5523734100f7cff8
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-10-30 11:21:50 +0400

    Add features

--- Client/lessons.js
+++ Client/lessons.js
@@ -1,33 +1,36 @@
 const lessons = [
     {
         id: 1,
-        title: "Math Tutoring",
+        title: "Maths",
         description: "Enhance your math skills!",
         price: 100,
         availableInventory: 5,
         image: "Images/maths.png",
         alt: "Math Lesson",
-        location: "Room 101" // New location property
+        location: "Room 101" ,// New location property
+        rating: 4 // New rating property
     },
     {
         id: 2,
-        title: "Science Workshop",
- description: "Hands-on science experience.",
+        title: "Science ",
+        description: "Hands-on science experience.",
         price: 150,
         availableInventory: 3,
         image: "Images/science.png",
         alt: "Science Workshop",
-        location: "Lab A" // New location property
+        location: "Lab A", // New location property
+        rating: 5 // New rating property
     },
     {
         id: 3,
-        title: "Place Physics Workshop",
+        title: " Physics",
         description: "Hands-on science experience.",
         price: 150,
         availableInventory: 6,
         image: "Images/science.png",
-        alt: "Physics Workshop",
-        location: "Auditorium" // New location property
+        alt: "Physics",
+        location: "Auditorium" ,// New location property
+        rating: 5 // New rating property
     }
     // Add more lessons as needed
 ];
