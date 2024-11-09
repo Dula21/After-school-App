8addd02f65dfa7410905406edb4852d7af005c43
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-11-09 19:01:28 +0400

    Made Some Changes

--- Client/lessons.js
+++ Client/lessons.js
@@ -35,7 +35,7 @@ const lessons = [
     {
     id: 4,
         title: "Arabic",
-        description: "Enhance your french skills!",
+        description: "Enhance your Arabic skills!",
         price: 100,
         availableInventory: 5,
         image: "Images/arabic.png",
@@ -51,6 +51,72 @@ const lessons = [
         price: 100,
         availableInventory: 5,
         image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 6,
+        title: "Chinese",
+        description: "Enhance your Chinese skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 7,
+        title: "Biology",
+        description: "Enhance your bio skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 8,
+        title: "Chemistry",
+        description: "Enhance your chemistry skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 9,
+        title: "History",
+        description: "Aware About old Generation!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/french.png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 10,
+        title: "Social Studies",
+        description: "Aware About Socitey!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/social-studies .png",
+        alt: "french Lesson",
+        location: "Room 101" ,// New location property
+        rating: 3 // New rating property
+    },
+    {
+        id: 11,
+        title: "Trade and Exports",
+        description: "Enhance your Trading skills!",
+        price: 100,
+        availableInventory: 5,
+        image: "Images/trade.png",
         alt: "french Lesson",
         location: "Room 101" ,// New location property
         rating: 3 // New rating property
